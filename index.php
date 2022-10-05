95e658c82ca750e6dd2f7842c779350c04280d26
Raul <nalvartediego40@gmail.com>
2022-10-05 11:15:45 -0500

    Mostrar Usuarios Terminado

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php include 'templates/header.php' ?>
 
+<?php 
+    include_once 'model/coneccion.php';
+
+    $sentencia = $bd -> query("select * from usuarios");
+    $persona = $sentencia->fetchAll(PDO::FETCH_OBJ);
+    // print_r($persona);
+?>
+
 <div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-4">
+        <div class="col-md-7">
             <div class="card">
                 <div class="card-header">
                     Usarios : 
@@ -12,6 +20,7 @@
                         <table class="table align-middle ">
                             <thead>
                                 <tr>
+                                    <th scope="col">id</th>
                                     <th scope="col">Nombre</th>
                                     <th scope="col">Apellidos</th>
                                     <th scope="col">Correo</th>
@@ -20,6 +29,23 @@
                             </thead>
                             <tbody>
                                 <tr class="">
+                                    <td>#</td>
+                                    <td scope="row">Diego</td>
+                                    <td>Nalvarte Yomona</td>
+                                    <td>nalr@gmail.com</td>
+                                    <td>Editar</td>
+                                    <td>Eliminar</td>
+                                </tr>
+                                <tr class="">
+                                    <td>#</td>
+                                    <td scope="row">Diego</td>
+                                    <td>Nalvarte Yomona</td>
+                                    <td>nalr@gmail.com</td>
+                                    <td>Editar</td>
+                                    <td>Eliminar</td>
+                                </tr>
+                                <tr class="">
+                                    <td>#</td>
                                     <td scope="row">Diego</td>
                                     <td>Nalvarte Yomona</td>
                                     <td>nalr@gmail.com</td>
@@ -36,11 +62,32 @@
         <div class="col-md-4">
             <div class="card">
                 <div class="card-header">
-                    Datos : 
+                    Ingresa usuario :
                 </div>
-                <div class="p-4">
-
+                <form action="registrarUsuario.php" method="POST" class="p-4">
+                <div class="mb-3">
+                    <label class="form-label">
+                        Nombre : 
+                    </label>
+                    <input type="text" class="form-control" name="txtNombre" autofocus>
                 </div>
+                <div class="mb-3">
+                    <label class="form-label">
+                        Apellidos : 
+                    </label>
+                    <input type="text" class="form-control" name="txtApellidos" autofocus>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">
+                        Correo : 
+                    </label>
+                    <input type="text" class="form-control" name="txtCorreo" autofocus>
+                </div>
+                <div class="d-grid">
+                    <input type="hidden" name="oculto"value="1">
+                    <input type="submit" class="btn btn-primary" value="Registrar">
+                </div>
+                </form>
             </div>
         </div>
     </div>
