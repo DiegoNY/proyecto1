ec81a9093758464f6456586a81ff54e76c2329a6
Raul <nalvartediego40@gmail.com>
2022-10-05 10:11:18 -0500

    Agregando tabla usuarios responsive

--- index.php
+++ index.php
@@ -7,12 +7,39 @@
                 <div class="card-header">
                     Usarios : 
                 </div>
+                <div class="p-4">
+                    <div class="table-responsive align-middle">
+                        <table class="table align-middle ">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Apellidos</th>
+                                    <th scope="col">Correo</th>
+                                    <th scope="col" colspan="2" >Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="">
+                                    <td scope="row">Diego</td>
+                                    <td>Nalvarte Yomona</td>
+                                    <td>nalr@gmail.com</td>
+                                    <td>Editar</td>
+                                    <td>Eliminar</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    
+                </div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card">
                 <div class="card-header">
                     Datos : 
+                </div>
+                <div class="p-4">
+
                 </div>
             </div>
         </div>
